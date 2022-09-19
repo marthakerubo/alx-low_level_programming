fa283b9ea7a4f6409a1c2c9a55c14be2453ed906
“marthakerubo” <marthakonduto01@gmail.com>
2022-09-19 15:20:57 -0700

    modified swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
 /**
  * swap_int - swaps the value of two integers
  *
- * @a: The first integer to be swapped
- * @b: The second integer to be swapped
+ * @a: int 1
+ * @b: int 2
  * Return: void
  */
 void swap_int(int *a, int *b);
