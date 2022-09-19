d419096fc7682f72da25287feb7b7594b04a4240
“marthakerubo” <marthakonduto01@gmail.com>
2022-09-19 16:58:37 -0700

    puts half string

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#inlude "main.h"
+
+/**
+ * swap_int - swaps values of two integers
+ * @a: first integer to be swapped
+ * @b: second integer to be swapped
+ * Return: success
+ */
+void swap_int(int *a, int *b)
+{
+
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
